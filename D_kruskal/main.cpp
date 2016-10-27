9bab4839c98d186c3c242178edfa06d78f3fdb08
MariaNosareva <mashunya_19-97@mail.ru>
2016-10-27 21:26:37 +0300

    Added tests.

--- D_kruskal/main.cpp
+++ D_kruskal/main.cpp
@@ -1,14 +1,46 @@
-  #include <iostream>
-  #include <fstream>
-  #include "Graph.h"
+#include <iostream>
+#include <fstream>
+#include "Graph.h"
+#include <gtest/gtest.h>
 
-  int main() {
-    std::ifstream in("kruskal.in");
-    Graph graph = Graph::FromStream(in);
-    in.close();
+TEST(TestingKruskal, SimpleGraph) {
+  std::ifstream in("/home/maria/Dropbox/prog/sem_3_c++/prog_mipt_clion/D_kruskal/files_for_tests/SimpleGraph");
+  Graph graph = Graph::FromStream(in);
+  in.close();
+  ASSERT_EQ(1, graph.MST());
+}
 
-    std::ofstream out("kruskal.out");
-    out << graph.MST();
-    out.close();
-    return 0;
-  }
+TEST(TestingKruskal, EqualEdgesTest) {
+  std::ifstream in("/home/maria/Dropbox/prog/sem_3_c++/prog_mipt_clion/D_kruskal/files_for_tests/EqualEdges");
+  Graph graph = Graph::FromStream(in);
+  in.close();
+  ASSERT_EQ(4, graph.MST());
+}
+
+TEST(TestingKruskal, DifficultGraph) {
+  std::ifstream in("/home/maria/Dropbox/prog/sem_3_c++/prog_mipt_clion/D_kruskal/files_for_tests/DifficultGraph");
+  Graph graph = Graph::FromStream(in);
+  in.close();
+  ASSERT_EQ(5, graph.MST());
+}
+
+TEST(TestingKruskal, MoreCycles) {
+  std::ifstream in("/home/maria/Dropbox/prog/sem_3_c++/prog_mipt_clion/D_kruskal/files_for_tests/MoreCycles");
+  Graph graph = Graph::FromStream(in);
+  in.close();
+  ASSERT_EQ(23, graph.MST());
+}
+
+TEST(TestingKruskal, RandomGraph) {
+
+  std::ifstream in("/home/maria/Dropbox/prog/sem_3_c++/prog_mipt_clion/D_kruskal/files_for_tests/FullGraph");
+  Graph graph = Graph::FromStream(in);
+  in.close();
+  ASSERT_EQ(6000, graph.MST());
+}
+
+
+int main(int argc, char ** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
