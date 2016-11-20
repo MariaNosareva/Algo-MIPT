719b396b180481dbd3a0e9956baa18a0544b02fc
MariaNosareva <mashunya_19-97@mail.ru>
2016-11-20 21:05:49 +0300

    one test added

--- F_Hamming/Tests/tests.cpp
+++ F_Hamming/Tests/tests.cpp
@@ -2,6 +2,18 @@
 #include "../NetworkLib/Network.h"
 #include <fstream>
 
+TEST(HammingTest, Example) {
+  std::string pattern = "1?", str = "00?";
+  Network net = Network::BuildNetwork(pattern, str);
+  net.MinCut();
+
+  ASSERT_EQ(2, net.GetHammingDistance());
+  ASSERT_EQ("10", net.GetPattern());
+  ASSERT_EQ("000", net.GetString());
+}
+
+//TODO more tests
+
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
