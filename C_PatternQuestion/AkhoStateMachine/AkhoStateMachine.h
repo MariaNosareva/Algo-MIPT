e24284e49d631b009f62491be0f3dfaadc765841
MariaNosareva <mashunya_19-97@mail.ru>
2016-12-08 23:02:34 +0300

    Solution of problem (non tested). Without tests.

--- C_PatternQuestion/AkhoStateMachine/AkhoStateMachine.h
+++ C_PatternQuestion/AkhoStateMachine/AkhoStateMachine.h
@@ -9,21 +9,30 @@
 #include <string>
 
 struct Vertex {
-  Vertex out[52]; //[a-zA-Z]
-  Vertex suffLink;
-  Vertex parent;
+  int out[52]; //[a-zA-Z]
+  int goes[52];
+  int suffLink;
+  int parent;
   char symbolIn;
   bool isTerminal;
+  std::vector<int> indicesOfSubpattern;
 };
 
 class AkhoStateMachine {
  private:
   std::string pattern;
+  std::string text;
   std::vector<Vertex> nodes;
  public:
   AkhoStateMachine();
+  static AkhoStateMachine FromStream(std::istream&);
   std::vector<int> getEndIndicesOfNonMaskSubpatterns();
+  std::vector<int> getStartIndicesOfNonMaskSubpatterns();
+  void addSubPattern(int, int, int);
   void BuildBor();
+  int getSuffLink(int);
+  int getLink(int, char);
+  std::vector<int> process();
 };
 
 #endif //PROJECT_AKHOSTATEMACHINE_H
